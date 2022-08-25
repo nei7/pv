dfee06cdc234495b0fd7ed4edc7dbbacc73bbcc8
nei7 <franciszek.szarek2@gmail.com>
2022-08-25 21:51:07 +0200

    feat: make clipboard works on linux

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,15 @@
 use std::process::Command;
 
+#[cfg(any(windows, target_os = "macos"))]
+pub fn copy_to_clipboard(s: &SafeString) -> Result<(), ()> {
+    use clipboard::ClipboardContext;
+    use clipboard::ClipboardProvider;
+
+    let mut context: ClipboardContext = ClipboardProvider::new().map_err(|_| ())?;
+    context.set_contents(s.deref().to_owned()).map_err(|_| ())?;
+    Ok(())
+}
+
 #[cfg(all(unix, not(target_os = "macos")))]
 pub fn copy_to_clipboard(password: &str) -> bool {
     let shell = format!(
